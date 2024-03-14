12c6a460c2a16e567351e566b2379953f26bff0c
andrey-shefa <andrey-shefa@tupalka.hustle.bg>
2024-03-14 11:20:28 +0200

    added firebase database

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+cred = credentials.Certificate('hakctuesx-firebase-adminsdk-2u8as-2928acf840.json')
+firebase_admin.initialize_app(cred)
+
 app = Flask(__name__)
+
+db = firestore.client()
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 @app.route('/')
 def hello_world():
+    doc_ref = db.collection(u'users').document(u'students')
+    doc_ref.set({
+        u'first': u'Ada',
+        u'last': u'Lovelace',
+        u'class': u'G'
+    })
     return 'Hello, World!'
 
 if __name__ == '__main__':
