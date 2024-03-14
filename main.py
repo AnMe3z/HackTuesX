bcb09a64703cf53bbe9eca79278a001b550731f9
andrey-d-dimitrov <andrey.d.dimitrov.2023@elsys-bg.org>
2024-03-14 13:11:32 +0200

    changes

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+import os
+from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+@app.route('/succes')
+def good():
+   return 'succes'
 
+
+@app.route('/submit',methods=["POST","GET"])
+def submit():
+    if(request.method=="POST"):
+     f = request.files['file']
+     f.save(f.filename)
+     return "file saved succesfully"
+    else: 
+     return render_template('submit.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
